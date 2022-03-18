851bba34a15157ff3041286106033e671d1612ed
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-03-18 05:11:03 +0300

    Fix efforts

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -16,7 +16,7 @@ namespace Redcode.Tweens
         {
             yield return new WaitForSeconds(1f);
 
-            yield return _target.DoJump(2f, new Vector3(0f, 5f, 3f), 0.7f).tween.SetLoopCount(int.MaxValue).SetLoopType(LoopType.Mirror).Play().WaitForComplete();
+            yield return _target.DoMoveAlongPath(_path, 1f).SetLoopCount(int.MaxValue).SetLoopType(LoopType.Continue).Play().WaitForComplete();
 
             print("Completed");
         }
